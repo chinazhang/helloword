9011f07869acbf6dc4c37ebda3f5f13f1a6fea8c
zhangfs <zhangfs@zhangfsdeMacBook-Air.local>
2019-06-30 21:43:31 +0800

    new add

--- Tkinter_study/chapter_one/Hello_world.py
+++ Tkinter_study/chapter_one/Hello_world.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """Hello World application for Tkinter"""
 from tkinter import *
 from tkinter.ttk import *
 root = Tk()
 label = Label(root, text="Hello World")
 label.pack()
+button = Button(root, text="click me")
+button.pack()
 root.mainloop()
